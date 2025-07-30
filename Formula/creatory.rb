e7e1c4fa668922de8ae3e22bc9ddf4a9660fc5ec
topbs <sasha.pleyzor.nyash@gmail.com>
2025-07-30 14:53:13 +0300

    sha

--- Formula/creatory.rb
+++ Formula/creatory.rb
@@ -2,7 +2,7 @@ class Creatory < Formula
   desc "CLI from Creatory Studio"
   homepage "https://github.com/topbs/creatory-cli"
   url "https://github.com/topbs/creatory-cli/archive/refs/tags/v0.1.tar.gz"
-  sha256 "362ee7145e11513dd21881bf8337aa75106eacaba56fe90a67cd780d3373912bcd "
+  sha256 "362ee7145e11513dd21881bf8337aa75106eacaba56fe90a67cd780d3373912b"
   license "MIT"
 
   def install
